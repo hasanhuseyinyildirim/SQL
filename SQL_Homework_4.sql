eb9ed3dd026bd55d4c831eebbfcc05417f7d6f78
Hasan Hüseyin Yıldırım <152974404+hasanhuseyinyildirim@users.noreply.github.com>
2025-05-30 19:35:25 +0300

    Add files via upload

---
+++ SQL_Homework_4.sql
@@ -0,0 +1,25 @@
+
+-- 1.
+--SELECT DISTINCT replacement_cost 
+--FROM film;
+
+
+-- 2.
+--SELECT COUNT (DISTINCT replacement_cost )
+--FROM film;
+--Cevap : 21
+
+-- 3.
+--SELECT COUNT (DISTINCT title) FROM film
+--WHERE title LIKE 'T%' AND rating = 'G';
+--Cevap : 9
+
+-- 4.
+--SELECT COUNT (DISTINCT country) FROM country
+--WHERE country LIKE '_____';
+--Cevap : 13
+
+-- 5.
+--SELECT COUNT (DISTINCT city) FROM city
+--WHERE city ILIKE '%r' 
+--Cevap : 33
