823281572a39a8a2957759b562d79a7b2e5be1e5
Hasan Hüseyin Yıldırım <152974404+hasanhuseyinyildirim@users.noreply.github.com>
2025-05-30 19:57:57 +0300

    Add files via upload

---
+++ SQL_Homework_6.sql
@@ -0,0 +1,29 @@
+
+-- 1.
+--SELECT ROUND (AVG(rental_rate),2) FROM film
+
+--Cevap : 2.98
+
+-- 2.
+--SELECT COUNT (title) FROM film
+--WHERE title LIKE 'C%';
+
+--Cevap : 92
+
+-- 3.
+--SELECT MAX(length) FROM film
+--WHERE rental_rate = 0.99;
+
+--Cevap : 184
+
+
+-- 4. 
+--SELECT COUNT (DISTINCT replacement_cost) FROM film
+--WHERE length > 150;
+
+--Cevap : 21
+
+
+
+
+
